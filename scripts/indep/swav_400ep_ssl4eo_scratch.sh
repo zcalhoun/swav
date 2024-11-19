0169949f356b75a528d6aa55fefe48a8f98cffec
Saad Lahrichi <79807776+slahrichi@users.noreply.github.com>
2024-11-19 17:48:30 -0600

    Update swav_400ep_ssl4eo_scratch.sh

--- scripts/indep/swav_400ep_ssl4eo_scratch.sh
+++ scripts/indep/swav_400ep_ssl4eo_scratch.sh
@@ -5,7 +5,7 @@ EXPERIMENT_PATH="/develop/results/ssl4eo_from_scratch_400/"
 
 mkdir -p $EXPERIMENT_PATH
 
-python -m torch.distributed.launch --nproc_per_node=8 main_swav.py \
+python -m torch.distributed.launch --nproc_per_node=4 main_swav.py \
 --data_path $DATASET_PATH \
 --task ssl4eo \
 --initialize_imagenet false \
@@ -25,7 +25,7 @@ python -m torch.distributed.launch --nproc_per_node=8 main_swav.py \
 --epoch_queue_starts 15 \
 --epochs 400 \
 --checkpoint_freq 50 \
---batch_size 32 \
+--batch_size 64 \
 --base_lr 0.6 \
 --final_lr 0.0006 \
 --freeze_prototypes_niters 5005 \
